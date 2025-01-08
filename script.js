979a6c779ae4bae68c5d26489f8c19da17788a11
kalyanam17abhinav <kalyanamabhinav83916@gmail.com>
2025-01-08 19:24:31 +0530

    update script

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const ham = document.getElementsByClassName('hamburger')[0];
-const nav = document.getElementsByClassName('navbar')[0];
-const list = document.getElementsByClassName('list')[0];
-const item = document.getElementsByClassName('item');
+const ham = document.querySelector('.hamburger');
+const nav = document.querySelector('.navbar');
+const list = document.querySelector('.list');
+const items = document.querySelectorAll('.item');
 
 ham.addEventListener('click', () => {
     nav.classList.toggle('expand');
     list.classList.toggle('expand');
-    for (let i = 0; i < item.length; i++) {
-        item[i].classList.toggle('expand');
-    }
-})
+    items.forEach((item) => {
+        item.classList.toggle('expand');
+    });
+});
