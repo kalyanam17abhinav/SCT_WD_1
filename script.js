b3c45a94f89aa4baf2839ce4307d03360c15f120
kalyanam17abhinav <kalyanamabhinav83916@gmail.com>
2025-01-08 22:40:51 +0530

    update home

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const ham = document.querySelector('.hamburger');
-const nav = document.querySelector('.navbar');
-const list = document.querySelector('.list');
-const items = document.querySelectorAll('.item');
+document.addEventListener('DOMContentLoaded', () => {
+    const ham = document.querySelector('.hamburger');
+    const nav = document.querySelector('.navbar');
+    const list = document.querySelector('.list');
+    const items = document.querySelectorAll('.item');
+    const home = document.querySelector('home');
 
-ham.addEventListener('click', () => {
-    nav.classList.toggle('expand');
-    list.classList.toggle('expand');
-    items.forEach((item) => {
-        item.classList.toggle('expand');
+    ham.addEventListener('click', () => {
+        nav.classList.toggle('expand');
+        list.classList.toggle('expand');
+        // item.classList.toggle('expand');
+        items.forEach((item) => {
+            item.classList.toggle('expand');
+        });
+        home.classList.toggle('expand');
     });
-});
+});1
